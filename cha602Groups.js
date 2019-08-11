a882f85685a3da2f3e94620c769e830dffa76df2
Bhagyashri <bhagyashri.nivdunge@sap.com>
2019-08-10 17:05:56 -0700

    Create cha602Groups.js

---
+++ cha602Groups.js
@@ -0,0 +1,33 @@
+
+class Group {
+    constructor() {
+      this.group = [];
+    }
+  
+    add(value) {
+      if (!this.has(value)) {
+        this.group.push(value);
+      }
+    }
+  
+    delete(value) {
+      this.group = this.group.filter(v => v !== value);
+    }
+  
+    has(value) {
+      return this.group.includes(value);
+    }
+  
+    static from(collection) {
+      let group = new Group;
+      for (let value of collection) {
+        group.add(value);
+      }
+      return group;
+    }
+  }
+  
+  
+  
+  let group = Group.from([10, 20]);
+  console.log(group.has(10));
